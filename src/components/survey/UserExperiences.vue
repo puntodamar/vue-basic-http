2b6d1f743d643fae382e04c2dec03f0eae9845d8
puntodamar <punto@privyid.tech>
2025-06-30 18:03:07 +0700

    add: get survey

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -7,7 +7,7 @@
       </div>
       <ul>
         <survey-result
-          v-for="result in results"
+          v-for="result in surveyResults"
           :key="result.id"
           :name="result.name"
           :rating="result.rating"
@@ -21,10 +21,42 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      surveyResults: []
+    }
+  },
+  mounted() {
+    this.fetchSurveyResults()
+  },
+  methods: {
+    async fetchSurveyResults() {
+      let data = null
+      const response = await fetch("https://vue-http-demo-85943-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json", {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok) {
+        data = await response.json()
+
+        for (const id in data) {
+          console.log(data[id])
+          this.surveyResults.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating,
+          })
+        }
+
+        console.log(this.surveyResults)
+      }
+    }
+  }
 };
 </script>
 
